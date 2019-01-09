556c38c5e7af5395ba1bd309304ad09eadab9119
RH <incredincomp@gmail.com>
2019-01-08 22:10:44 -0800

    Update and rename 1greprow.sh to greprow.sh

--- greprow.sh
+++ greprow.sh
@@ -1,7 +1,7 @@
 #!/bin/bash - 
 #===============================================================================
 #
-#          FILE: 1greprow.sh
+#          FILE: greprow.sh
 # 
 #         USAGE: sudo ./greprow.sh 
 # 
