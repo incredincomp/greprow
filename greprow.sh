570c91d13667067396f94e8bc6b0c7bf52dd533d
RH <incredincomp@gmail.com>
2019-07-24 14:37:32 -0700

    Update greprow.sh

--- greprow.sh
+++ greprow.sh
@@ -5,7 +5,8 @@
 # 
 #         USAGE: sudo ./greprow.sh 
 # 
-#   DESCRIPTION: Enter any information that could be contained in your list, this will
+#   DESCRIPTION:
+#		Enter any information that could be contained in your list, this will
 #		pull the entire line that is related to that search term and print/append the data
 #		to a new file with the name of the search issued.  needs format work,
 #		much apologies
@@ -15,14 +16,13 @@
 #		
 #          BUGS: will not work if you use a space in the search term, also, still creates a file even if script returns an error
 #                for no search term found, few others but gotta keep running it over and over yet
-#         NOTES: v2.0
+#         NOTES: v2.01
 #        AUTHOR: @incredincomp
 #  ORGANIZATION: 
 #       CREATED: 01/08/2019 09:55:54 PM
-#      REVISION:  /0/2019 10:40:00 AM
+#      REVISION:  07/24/2019 14:35:00 AM
 #===============================================================================
 
-set x
 set -o nounset                              # Treat unset variables as an error
 
 #this is just a weird function that I dont think i need.  at the end tho, part of the switch case calls for a repeat of the
@@ -34,34 +34,10 @@ next_Search () {
     next_Step
 }
 
-#function to ask user if they would like to define their own file path, if not, the program declares greprow/log.txt as input
-#yes_no () {
-#dialog --title "Define your own file/path?" \
-#--yesno "If you select no, $PWD/log.txt will be used." 7 60
-#response=$?
-#case $response in
-#   0)
-    #if yes/send user to set_Path dialog
-#               clear
-#               set_Path
-#               ;;
-#   1)
-    #if no/set predefined path
-#               clear
-#               echo "Okay, we set the path as $PWD\log.txt."
-#               FILEPATH="$PWD/log.txt"
-#               ;;
-#   255)
-#               clear
-#               echo "[ESC] key pressed."
-#               ;;
-#esac
-#}
-
 #set path has been reverted to command line interaction again, youre welcome to myself
 set_Path () {
 echo " If you would like to define your own path, please press y. Otherwise, if you want this program to break, please press n. "
-echo -n "y or n: " 
+echo -n " y or n: " 
 read answer
 case $answer in
 
@@ -81,7 +57,7 @@ case $answer in
 esac
 }
 
-#this function collects the variable that is used to search the specified file and stores it as lookFor
+# this function collects the variable that is used to search the specified file and stores it as lookFor
 what_Find () {  
     echo
     echo -n "What information would you like to find? (Do not use a Space if asking for a name.) "
@@ -94,7 +70,7 @@ what_Find () {
 }
 
 grep_Append () {
-#I dont know why this works, how or if it even should.  This while statement 
+# I dont know why this works, how or if it even should.  This while statement 
 # shows my naivety to bash scripting though.
 # DO NOT TOUCH!!!! THIS SHOULDNT WORK, SO THEREFORE ITS PERFECTLY BROKEN AS IS!
 
@@ -116,19 +92,19 @@ while :
  done
 }
 
-#This doesnt exist
-#Its just my way of tricking bash into doing what I want
+# This doesnt exist
+# Its just my way of tricking bash into doing what I want
 trick_Step () {
     next_Step
 }
 
-#this function is the final slide of the actual program. this will just ask if you would like to restart the program for another
-#search
+# this function is the final slide of the actual program. this will just ask if you would
+# like to restart the program for another search
 next_Step () {
 echo 
 echo -n "Would you like to run another search? [y or n]: "
 read reFind
-#this line prints a pretty --------- across the length of the terminal
+# this line prints a pretty --------- across the length of the terminal
 printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
 case $reFind in
    [yY] )
@@ -137,7 +113,7 @@ case $reFind in
 		     
    [nN] )
        echo "Okay, I hope you found me useful! See you next time!"
-#       printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
+       printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
        exit
        ;;
 	    
