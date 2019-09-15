9b3a4fe387728679d42954708e83824a245d8c0f
RH <incredincomp@gmail.com>
2019-09-14 23:27:15 -0700

    Some format and I fixed the if file exists or else on grep

--- greprow.sh
+++ greprow.sh
@@ -37,25 +37,18 @@
 #                 You should have received a copy of the GNU General Public License
 #                 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #===============================================================================
-
-
 clear
 set -o nounset                              # Treat unset variables as an error
 set -e
-
-# Print script in console for debug
+# Uncomment to print script in console for debug
 #set -xv
-
-## Global
-
+## Global Graphical Objects
 print_line () {
 printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
 }
-
 Banner () {
 	base64 -d <<<"ICAsLS0tLS4uICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgLC0uLS0tLS4gICAgICAgICAgICAgICAgICAgICAgICAgICAKIC8gICAvICAgXCAgICAgICAgICAgICAgICAgICAsLS4tLS0tLiAgXCAgICAvICBcICAgICAgICAgICAgICAgICAgICAgICAgICAKfCAgIDogICAgIDogICBfXyAgLC0uICAgICAgICBcICAgIC8gIFwgOyAgIDogICAgXCAgICwtLS0uICAgICAgICAgICAuLS0tLiAKLiAgIHwgIDsuIC8gLCcgLCcvIC98ICAgICAgICB8ICAgOiAgICB8fCAgIHwgLlwgOiAgJyAgICwnXCAgICAgICAgIC8uIC4vfCAKLiAgIDsgLy0tYCAgJyAgfCB8JyB8ICwtLS0uICB8ICAgfCAuXCA6LiAgIDogfDogfCAvICAgLyAgIHwgICAgIC4tJy0uICcgfCAKOyAgIHwgOyAgX18gfCAgfCAgICwnLyAgICAgXCAuICAgOiB8OiB8fCAgIHwgIFwgOi4gICA7ICwuIDogICAgL19fXy8gXDogfCAKfCAgIDogfC4nIC4nJyAgOiAgLyAvICAgIC8gIHx8ICAgfCAgXCA6fCAgIDogLiAgLycgICB8IHw6IDogLi0nLi4gJyAgICcgLiAKLiAgIHwgJ18uJyA6fCAgfCAnIC4gICAgJyAvIHx8ICAgOiAuICB8OyAgIHwgfCAgXCcgICB8IC47IDovX19fLyBcOiAgICAgJyAKJyAgIDsgOiBcICB8OyAgOiB8ICcgICA7ICAgL3w6ICAgICB8YC0nfCAgIHwgO1wgIFwgICA6ICAgIHwuICAgXCAgJyAuXCAgICAKJyAgIHwgJy8gIC4nfCAgLCA7ICcgICB8ICAvIHw6ICAgOiA6ICAgOiAgICcgfCBcLidcICAgXCAgLyAgXCAgIFwgICAnIFwgfCAKfCAgIDogICAgLyAgIC0tLScgIHwgICA6ICAgIHx8ICAgfCA6ICAgOiAgIDogOi0nICAgYC0tLS0nICAgIFwgICBcICB8LS0iICAKIFwgICBcIC4nICAgICAgICAgICBcICAgXCAgLyBgLS0tJy58ICAgfCAgIHwuJyAgICAgICAgICAgICAgICBcICAgXCB8ICAgICAKICBgLS0tYCAgICAgICAgICAgICAgYC0tLS0nICAgIGAtLS1gICAgYC0tLScgICAgICAgICAgICAgICAgICAgJy0tLSIgICAgICAKICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICA="
 }
-
 Credits () {
 	base64 -d <<<"U2NyaXB0IGNyZWF0ZWQgYW5kIG1haW50YWluZWQgYnkgQGluY3JlZGluY29tcCBvbiBodHRwczovL2dpdGh1Yi5jb20vaW5jcmVkaW5jb21wL2dyZXByb3cvCkNvbWUgYW5kIGpvaW4gdGhlIGNvbnZlcnNhdGlvbiAobWFpbmx5IGZpbGUgaXNzdWVzIGFuZCBmb3JrKQpGaWxlIGlzc3VlcyBoZXJlOiBodHRwczovL2dpdGh1Yi5jb20vaW5jcmVkaW5jb21wL2dyZXByb3cvaXNzdWVzClNwZWNpYWwgVGhhbmtzIHRvIEBWZW5vbTQwNCBmb3IgdGhlaXIgY29udHJpYnV0aW9ucyB0byB0aGUgYmFzZSBwcm9qZWN0IQpHcmVwUm93IENvcHlyaWdodCAoQykgMjAxOSAgQGluY3JlZGluY29tcApUaGlzIHByb2dyYW0gY29tZXMgd2l0aCBBQlNPTFVURUxZIE5PIFdBUlJBTlRZLgpUaGlzIGlzIGZyZWUgc29mdHdhcmUsIGFuZCB5b3UgYXJlIHdlbGNvbWUgdG8gcmVkaXN0cmlidXRlIGl0IHVuZGVyIGNlcnRhaW4gY29uZGl0aW9ucy4="
 }
@@ -68,9 +61,7 @@ Credits
 echo " "
 print_line
 }
-
 ## Menus
-
 Opening_Menu () {
 opener
 PS3='Please enter your choice: '
@@ -87,8 +78,7 @@ do
         "Delete the .txt Files")
 								echo "This will delete all .txt files in this directory"
 								read -r -e -p "Is this safe/okay? [y or n]: " dlt_ans
-								if [ "$dlt_ans" = "y" ]
-								then
+								if [[ "$dlt_ans" = "y" ]]; then
 									delete_Tests
 								elif [[ "$dlt_ans" = "n" ]]; then
 									return
@@ -104,7 +94,6 @@ do
     esac
 done
 }
-
 next_Step () {
 	print_line
 	PS3='What would you like to do now? '
@@ -155,9 +144,7 @@ next_Step () {
 		esac
 	done
 }
-
 ## Main
-# set path has been reverted to command line interaction again, youre welcome to myself
 set_Path () {
     printf "Please type your full file path, starting with a backslash if its absolute. "
     read -r -e -p "Its more than likely equal to $PWD/log.test: " inputPath
@@ -175,7 +162,6 @@ set_Path () {
 	    set_Path
     fi
 }
-
 # this function collects the variable that is used to search the specified file and stores it as Look_for
 what_Find () {
     print_line
@@ -186,7 +172,6 @@ what_Find () {
     echo "Looking for $Look_for... Please wait... Search started at:" && date -u
 		grep_Append
 }
-
 grep_Append () {
 # I dont know why this works, how or if it even should.  This while statement
 # shows my naivety to bash scripting though.
@@ -194,7 +179,7 @@ grep_Append () {
 while :
    do
      grep -i "$Look_for_clean" "$inputPath" >> "$Look_for_clean.txt" || echo "$Look_for_clean not found. Try something else."
-     if [ -s "./$Look_for_clean.txt" ]; then
+     if [ -f "./$Look_for_clean.txt" ]; then
 			 echo "$Look_for found and writing to file, check current directory for $Look_for.txt"
        echo "Search ended at " "$(date -u)"
        next_Step
@@ -202,22 +187,14 @@ while :
 			 delete_Miss
 			 echo "ERROR. Try a new Search"
        what_Find
-#     else
-#       echo "Error, $Look_for not found in specified file."
-#       delete_Miss
-#       print_line
-#       next_Step
      fi
    done
 }
-
 ## File Manipulation
-
 delete_Tests () {
     rm ./*.txt
     echo "Files deleted. Take care."
 }
-
 delete_Miss () {
 	rm "./$Look_for_clean.txt"
 }
@@ -225,13 +202,11 @@ get_ip () {
     echo "Check the current directory for a file name IPs-$Look_for_clean.txt"
     awk '{print $1}' "$PWD/$Look_for_clean.txt" | uniq -u > "IPs-$Look_for_clean.txt"
 }
-
 print_Content () {
     print_line
     print_File
     next_Step
 }
-
 print_File () {
 	FILE=$Look_for_clean.txt
 	cat "$FILE"
@@ -249,13 +224,11 @@ next_Search () {
     grep_Append
     next_Step
 }
-
 # This doesnt exist
 # Its just my way of tricking bash into doing what I want
 trick_Step () {
     next_Step
 }
-
 # ask if you would like to restart the program for another search
 last_Step () {
     print_line
@@ -278,5 +251,4 @@ last_Step () {
            ;;
     esac
 }
-
 Opening_Menu
